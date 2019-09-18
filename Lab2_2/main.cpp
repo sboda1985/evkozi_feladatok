a887b10122f04920234899ce2f86c3169f7e14aa
sboda1985 <bodaszilard@gmail.com>
2019-09-18 09:37:43 +0300

    2 es laborfeladat, a 2 es laborbol

---
+++ Lab2_2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double szam1, szam2, szam3;
+    cout << "szam1: ";
+    cin >> szam1;
+    cout << "szam2: ";
+    cin >> szam2;
+    cout << "szam3: ";
+    cin >> szam3;
+    cout << szam1 - szam2 + szam3 << endl;
+    return 0;
+}
